fa57acfd664f86efc5ac853818977ac7c6441452
Rajko97 <rmilanrajkovic@gmail.com>
2019-08-08 00:43:22 +0200

    Created simple authentication for home page

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
+var jwt = require('jsonwebtoken');
+var jwtVerifer = require('express-jwt');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -18,9 +21,28 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended:false}));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+var secret = "mySecret";
+
+app.post('/login', (req, res) => {
+  if(req.body.username == "milos.kosanovic" && req.body.password == "vts2019") {
+    let token = jwt.sign({userID: "milos.kosanovic"}, secret);
+    res.send(token);
+  } else {
+    res.sendStatus(400);
+  }
+});
+
+app.get('/', jwtVerifer({secret:secret}), (req, res) => {
+  res.send('Imas pristup sajtu');
+});
+
+app.use((err, req, res, next) => {
+  if(err.name === 'UnauthorizedError') {
+    res.send('Nemas pristup, saljem te na login formu. ('+err.message+')');
+  }
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
