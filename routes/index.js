7bf5e4aabeff817978e77b671fee607a85635d81
Rajko97 <rmilanrajkovic@gmail.com>
2019-08-22 16:28:58 +0200

    Imporoved orders controller

--- routes/index.js
+++ routes/index.js
@@ -4,22 +4,13 @@ const ordersModel = require('../model/orders');
 const createReformatedOrder = require('../controller/createReformatedOrder');
 
 router.get('/', function(req, res, next) {
-  waitForDataBeingReady()
-    .then(data => 
-      res.render('index', {title: 'VTS Restaurant', data:data})
-    );
-});
-
-function waitForDataBeingReady() {
-  return new Promise((res, rej) => {
-    ordersModel.find(async (err, docs) => {
-      const result = [];
-      for (const item of docs) {
-        result.push(await createReformatedOrder(JSON.parse(JSON.stringify(item))));  
-      }
-      res(result.reverse());    
-    });
+  ordersModel.find(async (err, docs) => {
+    const result = [];
+    for (const item of docs) {
+      result.push(await createReformatedOrder(JSON.parse(JSON.stringify(item))));  
+    }
+    res.render('index', {title: 'VTS Restaurant', data:result.reverse()});
   });
-}
+});
 
 module.exports = router;
--- routes/order.js
+++ routes/order.js
@@ -3,17 +3,17 @@ const router = express.Router();
 const ordersModel = require('../model/orders');
 const createReformatedOrder = require('../controller/createReformatedOrder');
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
     const io = require('../controller/socket').getio();
 
     const {tableId, orders} = req.body;
     const posted = Date.now();
     
-    createReformatedOrder({tableId, orders, posted}).then((data) => {
-        io.emit('orders', data);
-        ordersModel.collection.insertOne({tableId, posted, orders});
-        res.send('done');
-    });
+    const data = await createReformatedOrder({tableId, orders, posted})
+
+    io.emit('orders', data);
+    ordersModel.collection.insertOne({tableId, posted, orders});
+    res.send('done');
 });
 
 module.exports = router;
