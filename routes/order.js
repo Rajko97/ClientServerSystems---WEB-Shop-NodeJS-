10e14a3191131b0db1f8879ed01262797366bd23
Rajko97 <rmilanrajkovic@gmail.com>
2019-08-28 20:49:56 +0200

    Created reading tableID from token

--- routes/order.js
+++ routes/order.js
@@ -2,18 +2,22 @@ const express = require('express');
 const router = express.Router();
 const ordersModel = require('../model/orders');
 const createReformatedOrder = require('../controller/createReformatedOrder');
+const jwt = require('jsonwebtoken');
+const constants = require('../constants');
 
 router.post('/', async (req, res) => {
     const io = require('../controller/socket').getio();
+    const token = req.headers.authorization.slice(7, req.headers.authorization.length).trimLeft();
+    const decoded = jwt.verify(token, constants.jwt_secret);
 
-    const {tableId, orders} = req.body;
+    const tableId = decoded.tableID;
+    const orders = req.body.orders;
     const posted = Date.now();
-    
     const data = await createReformatedOrder({tableId, orders, posted})
-
+    
     io.emit('orders', data);
     ordersModel.collection.insertOne({tableId, posted, orders});
-    res.send('done');
+    res.send('{"message":"done"}');
 });
 
 module.exports = router;
