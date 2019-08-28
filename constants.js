36c5931b2322a00e0fb5a9b10492365be0973979
Rajko97 <rmilanrajkovic@gmail.com>
2019-08-28 20:49:15 +0200

    Added Port in constants and server url in console

--- constants.js
+++ constants.js
@@ -1,12 +1,13 @@
 const shouldFillDBWithData = true;
-const dbPath = 'mongodb://localhost:27017/shopify2';
-
+const dbPath = 'mongodb://localhost:27017/shop';
 const jwt_secret = "HW4@sfT^75xcAHT12ZUHS"; 
 const loginPasswordForClient = "vts2019";
 const loginPasswordForWorker = "vts2019";
 const loginUsernameForWorker = "radnik";
+const port = '3000';
 
 module.exports = {
+    PORT : port,
     dbPath : dbPath,
     initalizeDB : shouldFillDBWithData,
     jwt_secret : jwt_secret,
